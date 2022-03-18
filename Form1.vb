fb4879cfb6c310e1f444cf83fc59bbe975b03bb3
kellymuterian <muteriank17@gmail.com>
2022-03-18 11:29:47 +0300

    delete

--- Form1.vb
+++ Form1.vb
@@ -37,6 +37,10 @@ Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Application.Exit()
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 =======
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
